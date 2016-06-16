de032bd15674d83488cf2e369f4eed944b8c4e86
Kyle Cook <kylecook80@gmail.com>
2016-06-15 19:37:31 -0500

    Added helper scripts for getting the application setup

---
+++ deps.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+OS=$(uname | tr 'A-Z' 'a-z')
+
+if [[ $OS = 'darwin' ]]; then
+    if [[ ! `type "brew"` > /dev/null ]]; then
+        echo "Please install homebrew first."
+    else
+        brew install postgresql redis hiredis cppcheck doxygen cmake valgrind
+    fi
+elif [[ $OS = 'linux' ]]; then
+    sudo apt-get -y install postgresql postgresql-contrib libpq-dev \
+        redis-server libhiredis-dev cppcheck clang valgrind \
+        doxygen cmake
+else
+    echo "Your operating system is not currently not supported."
+fi
