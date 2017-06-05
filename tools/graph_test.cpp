360a5c2dd7dcc50d82904de88f28c40ef540a062
Kyle Ruff <ruff@debian.kruff.local>
2017-06-05 14:51:07 -0500

    added graph_test.cpp, a test program that uses the boost::graph library to build a simple graph, to the tools subdirectory

---
+++ tools/graph_test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+#include <boost/graph/graph_traits.hpp>
+
+struct Ve {
+    std::string name;
+};
+struct Ed {
+    int cost;
+};
+
+int main() {
+
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::directedS, Ve, Ed> Graph;
+    typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef boost::graph_traits<Graph>::edge_descriptor Edge;
+
+    Graph g;
+    Vertex a = boost::add_vertex(g);
+    Vertex b = boost::add_vertex(g);
+    Vertex c = boost::add_vertex(g);
+
+    Edge e1,e2; bool added;
+    boost::tie(e1, added) = boost::add_edge(a,b,g);
+    boost::tie(e2, added) = boost::add_edge(a,c,g);
+
+    g[a].name = "First";
+    g[b].name = "Second";
+    g[c].name = "Last";
+
+    std::ofstream gout;
+    gout.open("test1.dot");
+    boost::dynamic_properties dp;
+    dp.property("label", get(&Ve::name, g));
+    dp.property("node_id", get(boost::vertex_index, g));
+    boost::write_graphviz_dp(gout, g, dp);
+    
+}
+
