eda9c7395969465fffd5679a757e514a3385b9e6
Tabor Kvasnicka <kamakwazee@gmail.com>
2018-03-09 14:21:24 -0600

    Added libconfig to deps.sh

--- deps.sh
+++ deps.sh
@@ -6,21 +6,21 @@ if [[ $OS = 'darwin' ]]; then
     if hash brew 2>/dev/null; then
         xcode-select --install > /dev/null
         brew cask install postgres
-        brew install postgresql cmake boost cppcheck doxygen graphviz tbb
+        brew install postgresql cmake boost cppcheck doxygen graphviz tbb libconfig
     else
         echo "Please install homebrew first."
     fi
 elif [[ $OS = 'linux' ]]; then
     if hash pacman 2>/dev/null; then
-        sudo pacman -S cmake postgresql boost cppcheck clang doxygen graphviz
+        sudo pacman -S cmake postgresql boost cppcheck clang doxygen graphviz libconfig
     elif hash apt-get 2>/dev/null; then
         sudo apt-get -y install libboost-graph-dev postgresql postgresql-contrib \
             libpq-dev cppcheck clang valgrind doxygen graphviz cmake \
-            build-essential bison flex libssl-dev
+            build-essential bison flex libssl-dev libconfig
     elif hash yum 2>/dev/null; then
         sudo yum install cmake epel-release postgresql-devel boost-devel \
             openssl-devel graphviz-devel bison-devel flex-devel cppcheck valgrind \
-            tbb
+            tbb libconfig-devel
     else
         echo "Your distro is currently not supported."
     fi
