ba1c0e80dfffb821cb7b75c37f1a80d0eeff83d9
Tabor Kvasnicka <kamakwazee@gmail.com>
2018-07-06 15:04:36 -0500

    Removed test lua script

--- redis_scripts/broadcast.lua
+++
@@ -1,9 +0,0 @@
-local count = 0
-
-local broadcast = redis.call("lrange", KEYS[1], 0, -1)
-
-for _, key in ipairs(broadcast) do
-    redis.call("incr", key)
-    count = count + 1
-end
-return count
